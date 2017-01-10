84088bedd6d7480024f080f268fd187e6643959d
gord chung <gord@live.ca>
2017-01-10 18:09:58 +0000

    move integration to ceilometer
    similar to how gnocchi is set up
    
    Change-Id: Iaf83100aa2226ef159810e878ef1a652f1d8aea1

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -179,10 +179,6 @@ function configure_panko {
     if is_service_enabled panko-api && [ "$PANKO_USE_MOD_WSGI" == "True" ]; then
         _panko_config_apache_wsgi
     fi
-
-    if is_service_enabled ceilometer-collector; then
-        iniadd $CEILOMETER_CONF DEFAULT event_dispatchers panko
-    fi
 }
 
 # init_panko() - Initialize etc.
