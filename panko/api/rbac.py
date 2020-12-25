6da394a59a9bf03c91dc53e4db8efe05ffa6c5ad
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-12-24 21:51:04 -0600

    [goal] Deprecate the JSON formatted policy file
    As per the community goal of migrating the policy file
    the format from JSON to YAML[1], we need to do two things:
    
    1. Change the default value of '[oslo_policy] policy_file''
    config option from 'policy.json' to 'policy.yaml' with
    upgrade checks.
    
    2. Deprecate the JSON formatted policy file on the project side
    via warning in doc and releasenotes.
    
    Also replace policy.json to policy.yaml ref from doc and tests.
    
    [1]https://governance.openstack.org/tc/goals/selected/wallaby/migrate-policy-format-from-json-to-yaml.html
    
    Change-Id: Ie6e22b0b47c5148290f7b76c95967cd9a343343c

--- panko/api/rbac.py
+++ panko/api/rbac.py
@@ -16,12 +16,20 @@
 
 """Access Control Lists (ACL's) control access the API server."""
 
+from oslo_config import cfg
+from oslo_policy import opts
 from oslo_policy import policy
 import pecan
 
 from panko import policies
 
 _ENFORCER = None
+
+# TODO(gmann): Remove setting the default value of config policy_file
+# once oslo_policy change the default value to 'policy.yaml'.
+# https://github.com/openstack/oslo.policy/blob/a626ad12fe5a3abd49d70e3e5b95589d279ab578/oslo_policy/opts.py#L49
+DEFAULT_POLICY_FILE = 'policy.yaml'
+opts.set_defaults(cfg.CONF, DEFAULT_POLICY_FILE)
 
 
 def init():
